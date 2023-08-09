aae1b7948979011e6cdad025440d862ff32e064d
Arnaudb78 <arnaudb92230@gmail.com>
2023-08-09 19:09:27 +0200

    project password generator

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Générateur de mot de passe</h1>
+    <form action="listPassword.php">
+        Combien de mot de passe ? : <input type="number" name="nbrPasswd" min="1" max="50" value="1">
+        <br>
+        La taille du mot de passe ? : <input type="number" name="nbrCaract" min="1" max="50" value="10">
+        <br>
+        Type de mot de passe : <select name="typePasswd" id="">
+            <option value="1">Chiffres uniquement</option>
+            <option value="2">Lettres uniquement</option>
+            <option value="3">Chiffres et lettres</option>
+            <option value="4">Tout caractères</option>
+        </select>
+        <br>
+        <input type="submit" value="Générer" class="btn">
+    </form>
+
+    
+</body>
+</html>
---
+++ listPassword.php
@@ -0,0 +1,36 @@
+<?php 
+//Récupère les params pour adapter aux besoins du User
+$saisieNbrPasswd = $_GET['nbrPasswd'];
+$saisieNbrCaract = $_GET['nbrCaract'];
+$saisieTypePasswd = $_GET['typePasswd'];
+
+// Type de caractères à prendre en compte pour générer les mots de passe (change selon les params du User)
+if($saisieTypePasswd == '1'){
+    $caract = "0123456789";
+}else if($saisieTypePasswd =='2') {
+    $caract = "abcdefghijklmnopqrstuvwxyz";
+} else if($saisieTypePasswd =='3'){
+    $caract = "abcdefghijklmnopqrstuvwxyz0123456789";
+} else if($saisieTypePasswd =='4'){
+    $caract = "abcdefghijklmnopqrstuvwxyz0123456789@!:;,$/?*£=+-";
+}
+
+//Nombre de caract que le mdp doit contenir (-saisie du user)
+$nb_caract = $saisieNbrCaract;
+
+// Je fais une boucle pour générer des mdp jusqu'au nb indiqué par le user
+// puis une seconde boucle pour générer le mdp caract par caract jusqu'au nb indiqué par le user
+for($nbrPasswd = 1; $nbrPasswd<= $saisieNbrPasswd; $nbrPasswd++){
+    for($i = 1; $i <= $nb_caract; $i++){
+        //on compte le nombre de caract
+        $Nbr = strlen($caract);
+
+        //On choisit un caract au hasard dans la chaine sélectionnée 
+        $Nbr = mt_rand(0,($Nbr-1));
+
+        // POur finir j'écris le resultat
+        print $caract[$Nbr];
+    }
+    echo "<br>";
+} 
+?>
